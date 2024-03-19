0144ddb220796804e4557341baef243fbb0739f8
Jakob Givoni <jgivoni@users.noreply.github.com>
2024-03-19 13:44:42 +0100

    1-install-more-infrastructure

---
+++ .php-cs-fixer.php
@@ -0,0 +1,16 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = (new Finder())
+    ->in(__DIR__ . '/src');
+
+return (new Config())
+    ->setRules([
+        '@PSR12' => true,
+        '@Symfony' => true,
+        'phpdoc_to_comment' => false,
+        'concat_space' => 'one',
+    ])
+    ->setFinder($finder);
